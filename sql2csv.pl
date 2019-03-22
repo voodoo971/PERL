5094a5b78b81eea53f472c742efdb60e99a547eb
voodoo971 <33722656+voodoo971@users.noreply.github.com>
2019-03-22 14:13:18 +0100

    Update sql2csv.pl

--- sql2csv.pl
+++ sql2csv.pl
@@ -4,34 +4,50 @@ use strict;
 use 5.010;
 use Data::Dumper qw(Dumper);
 
-open(FILE, "<XXX.sql") || die "File not found";
+open(FILE, "<ResultText_201903172138.sql") || die "File not found";
 my @lines = <FILE>;
 close(FILE);
 
-open(OUT, ">XXX.tmp") || die "File not found";
+open(OUT, ">ResultText_201903172138.tmp") || die "File not found";
+
+my $filename = '/var/log/sql2csv.log';
+open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+
+print $fh "Processing create tmp file ...\n";
 
 foreach my $line1 (@lines)
 {
-
 my @tab = split /VALUES/, $line1;
-
-
 foreach my $line2 (@tab)
 {
-   $line2 =~ s/,/ /g;
-$line2 =~ s/'//g;
-   if ($line2 !~ m/INSERT/ )
+
+   $line2 =~ s/"/ /g;
+   $line2 =~ s/\\/\\\\/g;
+   $line2 =~ s/'//g;
+   $line2 =~ s/NULL/0/g;
+   if ($line2 !~ m/INSERT/)
    {
 
    print OUT $line2;
+print $fh "Processing create tmp file ...\n";
  }
+
 }
+
 }
+
+
 
 
 close(OUT);
 
+print $fh "Processing create csv...\n";
 
-system "cat XXX.tmp | tr -d ';' | tr -d ')' | tr -d '(' > XXX.csv";
+system "cat ResultText_201903172138.tmp | tr -d ';' | tr -d ')' | tr -d '(' > ResultText.csv";
+my $cmd = 'sed -i "/^[ \t]*$/d" ResultText.csv';
+system($cmd);
 
+print "[ Finish ] >>>>>>>>>>>>>> [OK ] \n";
+
+close $fh;
 exit;
