9b15d6e52393fd92aac2cd45c99e0c6f7fe19faa
voodoo971 <33722656+voodoo971@users.noreply.github.com>
2019-03-22 15:47:58 +0100

    Update sql2csv.pl

--- sql2csv.pl
+++ sql2csv.pl
@@ -3,51 +3,72 @@ use warnings;
 use strict;
 use 5.010;
 use Data::Dumper qw(Dumper);
+use Cwd qw(cwd);
 
-open(FILE, "<ResultText_201903172138.sql") || die "File not found";
-my @lines = <FILE>;
-close(FILE);
+sub generate_csv {
 
-open(OUT, ">ResultText_201903172138.tmp") || die "File not found";
 
-my $filename = '/var/log/sql2csv.log';
-open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+  my ($file_sql) = @_;
 
-print $fh "Processing create tmp file ...\n";
 
-foreach my $line1 (@lines)
-{
-my @tab = split /VALUES/, $line1;
-foreach my $line2 (@tab)
-{
 
-   $line2 =~ s/"/ /g;
-   $line2 =~ s/\\/\\\\/g;
-   $line2 =~ s/'//g;
-   $line2 =~ s/NULL/0/g;
-   if ($line2 !~ m/INSERT/)
-   {
+(my $without_extension = $file_sql) =~ s/.sql//g;
+(my $ext_tmp = $file_sql) =~ s/.sql/.tmp/g;
+(my $ext_csv = $file_sql) =~ s/.sql/.csv/g;
 
+  open(FILE, "<$file_sql") || die "File not found";
+  my @lines = <FILE>;
+  close(FILE);
+  open(OUT, ">$ext_tmp") || die "File not found";
+  my $filename = '/var/log/sql2csv.log';
+  open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+  print $fh "Processing create tmp file ...\n";
+  foreach my $line1 (@lines)
+  {
+  my @tab = split /VALUES/, $line1;
+  foreach my $line2 (@tab)
+  {
+    $line2 =~ s/"/ /g;
+     $line2 =~ s/\\/\\\\/g;
+     $line2 =~ s/'//g;
+     $line2 =~ s/NULL/0/g;
+     if ($line2 !~ m/INSERT/)
+     {
    print OUT $line2;
-print $fh "Processing create tmp file ...\n";
- }
+  print $fh "Processing create tmp file from file : $file_sql\n";
+   }
+  }
+  }
+  close(OUT);
+
+
+
+
+  print $fh "Processing create csv from file : $ext_csv \n";
+
+chomp($ext_tmp);
+
+print "[ Processing ] création en cours fichier csv [ $ext_csv ]";
+my $s = "cat $ext_tmp | tr -d ';' | tr -d ')' | tr -d '(' > $ext_csv";
+
+  system ($s);
+  my $cmd = "sed -i \"\/\^\[ \\t\]\*\$\/\d\" $ext_csv";
+    system ($cmd);
+  print "[ Finish ] >>>>>>>création fichier csv [ $ext_csv ]>>>>> [OK ]";
+  close $fh;
 
 }
 
+my $path = cwd;
+
+
+my @file_path = `ls $path | grep \.sql`;
+
+foreach my $list_file_sql (@file_path)
+{
+
+generate_csv($list_file_sql );
+
 }
 
-
-
-
-close(OUT);
-
-print $fh "Processing create csv...\n";
-
-system "cat ResultText_201903172138.tmp | tr -d ';' | tr -d ')' | tr -d '(' > ResultText.csv";
-my $cmd = 'sed -i "/^[ \t]*$/d" ResultText.csv';
-system($cmd);
-
-print "[ Finish ] >>>>>>>>>>>>>> [OK ] \n";
-
-close $fh;
 exit;
